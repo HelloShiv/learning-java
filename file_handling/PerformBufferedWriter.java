249253b27203ae09fadd4a8eae4c92b5a50d42f5
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-17 10:55:33 +0530

    BufferedReader setup

--- file_handling/PerformBufferedWriter.java
+++ file_handling/PerformBufferedWriter.java
@@ -1,16 +1,19 @@
 import java.io.*;
 
-public class PerformFileWriteOperation {
+public class PerformBufferedWriter  {
     public static void main(String[] args) throws Exception {
         
-        FileWriter fWriter = new FileWriter("C:\\Users\\awdhe\\Desktop\\learning\\Java\\file_handling\\test.txt");
+        FileWriter writer = new FileWriter("C:\\Users\\awdhe\\Desktop\\learning\\Java\\file_handling\\test.txt");  
+        BufferedWriter fWriter = new BufferedWriter(writer);
         try{
             fWriter.write("Java programming is ...");
         }
         finally{
+            
             fWriter.close();
         }
         System.out.println("Successfully Data wrote in File");
     }
     
 }
+
