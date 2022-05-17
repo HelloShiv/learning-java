de4f9e049c516f4544a98565159a14775281f671
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-17 22:39:04 +0530

    Contains Wrapper Class Readme file

---
+++ wrapper_class/WrapperClass.java
@@ -0,0 +1,10 @@
+public class WrapperClass {
+    public static void main(String[] args){
+        int x =32;
+        Integer y = Integer.valueOf("23");
+        int copy = y.intValue();
+        System.out.println(x+"\n"+y);
+        System.out.println(y.getClass().getSimpleName());
+        System.out.println("Copy: "+ copy);
+    }
+}
