0ebb53cc922e293c2ddc5fd7e077582f5800e43b
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-17 12:28:45 +0530

    writing name in the file

---
+++ file_handling/sir.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class sir {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter  Number: ");
+        String name = scan.next();
+
+        FileOutputStream fout = new FileOutputStream("something.txt");
+
+        byte[] something1 = name.getBytes();
+
+        fout.write(something1);
+
+        fout.close();
+
+    }
+}
