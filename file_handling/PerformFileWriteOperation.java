080946bac62e10c007e3673aa24241126137d568
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-17 10:42:04 +0530

    Basic file write operation

---
+++ file_handling/PerformFileWriteOperation.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class PerformFileWriteOperation {
+    public static void main(String[] args) throws Exception {
+        
+        FileWriter fWriter = new FileWriter("C:\\Users\\awdhe\\Desktop\\learning\\Java\\file_handling\\test.txt");
+        try{
+            fWriter.write("Java programming is ...");
+        }
+        finally{
+            fWriter.close();
+        }
+    }
+    
+}
