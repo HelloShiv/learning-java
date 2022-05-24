a3551f25cb8983b1ef8d20c1883dbac64c428a73
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-24 11:56:02 +0530

    MultiThreading demo

---
+++ multithreading/MultiThreadingDemo.java
@@ -0,0 +1,22 @@
+// we can use the thread as object or directly extend the class this is called thread class
+// Using interface as thread
+
+public class MultiThreadingDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        t.setName("My Thread");
+        System.out.println("Thread name: "  + t.getName());
+        System.out.println("After name change: " + t);
+
+        try{
+            for(int n=5; n>0; n--){
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } 
+        catch (InterruptedException e){
+            System.out.println("Main thread iterrupted");
+        }
+        
+    }
+}
