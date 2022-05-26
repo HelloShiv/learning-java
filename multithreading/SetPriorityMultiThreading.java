ab8972beb4448ab2c98e607709e1b6c0efcdb5e3
HelloShiv <swagatswaroopparida@gamil.com>
2022-05-26 11:31:04 +0530

    Mutlti-Threading setting Priority

---
+++ multithreading/SetPriorityMultiThreading.java
@@ -0,0 +1,37 @@
+// WAP and show Threading using Runnable
+class demo implements Runnable {
+    String name;
+    Thread t;
+
+    demo(String s,int a) {
+        name = s;
+        t = new Thread(this, name);
+        System.out.println("New Thread: " + t);
+        t.setPriority(a);
+        t.start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 2; i <= 20; i += 2) {
+                System.out.print(i + " ");
+                Thread.sleep(500);
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("\nExisting thread: " + name);
+    }
+}
+
+public class SetPriorityMultiThreading {
+    public static void main(String[] args) {
+        new demo("One",5);
+        new demo("Two",10);
+        new demo("Three",1);
+
+    }
+}
+
+// Default priority is 5
